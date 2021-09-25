f829b6bcf0578f9da4736b69f4614757a370d63d
Woodchucks <37177696+Woodchucks@users.noreply.github.com>
2021-09-25 15:29:05 +0200

    Complete code Silent_Auction_WIP

--- Silent_Auction_WIP.py
+++ Silent_Auction_WIP.py
@@ -1,29 +1,27 @@
 from replit import clear
-#call clear() to clear the output in the console.
 from art import logo
 print(logo)
 
 next_bidder = True
-bidders = []
 bidder = {}
 while next_bidder == True:
+  clear()
   name = input("What's your name?")
-  price = input("What's your price?")
+  price = int(input("What's your price?"))
   next_person = input("Is there another person who would like to bet?")
 
   def add_bidder(name, price_auction):
     bidder[name] = price_auction
-    bidders.append(bidder)
   
   add_bidder(name, price)
 
   if next_person.lower() == "no":
     next_bidder = False
 
-# max_price = 0
-# for item in bidders:
-#   if bidders[item][0] > bidders[item+1][0]:
-#     max_price = bidders[item][0]
-#   else:
-#     max_price = bidders[item + 1][0]
-# print(bidders[1])
+max_price = 0
+bidder_name = ''
+for item in bidder:
+  if max_price < bidder[item]:
+    max_price = bidder[item]
+    bidder_name = item
+print(f"The winner is {bidder_name} with amount {max_price} PLN")
