a76cc0bf192325537693b6562f01309b28bdd24e
Woodchucks <37177696+Woodchucks@users.noreply.github.com>
2021-10-14 23:36:13 +0200

    Create coffeeMachineOOP.py

---
+++ coffeeMachineOOP.py
@@ -0,0 +1,20 @@
+//other files on PC resources
+
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+coffee_maker = CoffeeMaker()
+money_machine = MoneyMachine()
+menu = Menu()
+
+finalized = False
+while not finalized:
+  choice = input(f"What would you like? {menu.get_items()} ")
+  drink = menu.find_drink(choice)
+  if choice == "report":
+    coffee_maker.report()
+  else:
+    if coffee_maker.is_resource_sufficient(drink):
+      if money_machine.make_payment(drink.cost):
+        coffee_maker.make_coffee(drink)
