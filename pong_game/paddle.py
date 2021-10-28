58c66ca0ab43ba17a491acf53bfc9967c152f926
Woodchucks <37177696+Woodchucks@users.noreply.github.com>
2021-10-28 19:55:52 +0200

    Create paddle.py

---
+++ pong_game/paddle.py
@@ -0,0 +1,18 @@
+from turtle import Turtle
+
+class Paddle(Turtle):
+    def __init__(self, position):
+        super().__init__()
+        self.shape("square")
+        self.shapesize(stretch_wid=4, stretch_len=0.5)
+        self.penup()
+        self.color("white")
+        self.goto(position)
+
+    def move_up(self):
+        new_y = self.ycor() + 20
+        self.goto(self.xcor(), new_y)
+
+    def move_down(self):
+        new_y = self.ycor() - 20
+        self.goto(self.xcor(), new_y)
