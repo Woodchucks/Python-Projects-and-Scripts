4c5c318d02a5e5cc00f2e4f107a188831c080a7f
Woodchucks <37177696+Woodchucks@users.noreply.github.com>
2021-10-28 19:56:11 +0200

    Create ball.py

---
+++ pong_game/ball.py
@@ -0,0 +1,28 @@
+from turtle import Turtle
+
+class Ball(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape("circle")
+        self.color("white")
+        self.filling()
+        self.penup()
+        self.x_cor = 10
+        self.y_cor = 10
+        self.current_speed = 0.1
+
+    def move(self):
+        new_x = self.xcor() + self.x_cor
+        new_y = self.ycor() + self.y_cor
+        self.goto(new_x, new_y)
+
+    def bounce_y(self):
+        self.y_cor *= -1
+
+    def bounce_x(self):
+        self.x_cor *= -1
+
+    def reset_position(self):
+        self.goto(0, 0)
+        self.bounce_x()
+        self.current_speed = 0.1
