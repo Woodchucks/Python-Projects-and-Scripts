57a8a6b1475a6fd0acb6960c744d083b00b2c0cf
Woodchucks <37177696+Woodchucks@users.noreply.github.com>
2022-02-07 00:22:39 +0100

    Add files via upload

--- Blog_with_flask/main.py
+++ Blog_with_flask/main.py
@@ -1,1 +1,25 @@
-main
+from flask import Flask, render_template
+import requests
+
+app = Flask(__name__)
+response = requests.get("https://api.npoint.io/42e3519dde93dbe96c91")
+posts = response.json()
+
+@app.route('/')
+def home():
+    return render_template('index.html', posts=posts)
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/post/<int:id>')
+def post(id):
+    return render_template('post.html', post=posts[id-1])
+
+if __name__ == '__main__':
+    app.run(debug=True)
