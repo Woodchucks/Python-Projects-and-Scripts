932acca3c32b25b67b340e820fed80fb798b2442
Woodchucks <37177696+Woodchucks@users.noreply.github.com>
2022-06-01 14:31:56 +0200

    Update primeNumbers.py

--- primeNumbers.py
+++ primeNumbers.py
@@ -1,8 +1,18 @@
-def prime_checker(number):
-  if(number%2 == 0 or number%3 == 0 or number%5 == 0 or number%7 == 0 or number%9 == 0):
-    print("It's not a prime number.")
-  else:
-    print("It's a prime number.")
-
-n = int(input("Check this number: "))
-prime_checker(number=n)
+def is_prime(number):
+    if number <= 1:
+        return "Insert number greater than 1."
+    elif number in [2, 3, 5, 7]:
+        return "Prime number"
+    else:
+      if(number%2 == 0 or number%3 == 0 or number%5 == 0 or number%7 == 0):
+        return "Not a prime number"
+      else:
+        return "Prime number"
+      
+def test_is_prime():
+    assert is_prime(13) == "Prime number"
+    assert is_prime(2) == "Prime number"
+    assert is_prime(3) == "Prime number"
+    assert is_prime(1) == "Insert number greater than 1."
+    assert is_prime(25) == "Not a prime number"
+    assert is_prime(12) == "Not a prime number"
